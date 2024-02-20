82e548cb2420f1138c2beb65a5001b8f9d8a659b
sneakysnail187 <maduvivier53@gmail.com>
2024-02-20 14:53:10 -0500

    Update interpreter.rkt
    small edits still figuring out state and value

--- interpreter.rkt
+++ interpreter.rkt
@@ -6,12 +6,25 @@
 (define operand2 caddr)
 (define operand3 cadddr)
 (define bComparator (lambda (exp) (cons (car exp) (cons (cadr exp) '()))))
+(define isVar
+  (lambda (x)
+    (cond
+      [(number? x) #f]
+      [(eq? '+ x) #f]
+      [(eq? '- x) #f]
+      [(eq? '/ x) #f]
+      [(eq? '* x) #f]
+      [(eq? '% x) #f]
+      [(list?  x) #f]
+      [else       #t])))
+  
 
 ;; Operations on 2 integers
 (define M_integer
-  (lambda (lis)
+  (lambda (lis state)
     (cond
       [(number? lis) lis]
+      [(isVar lis)   (cdr (getBinding lis state))]
       [(eq? '+ (operator lis)) (+ (M_integer(operand1 lis)) (M_integer(operand2 lis)))]
       [(eq? '- (operator lis)) (- (M_integer(operand1 lis)) (M_integer(operand2 lis)))]
       [(eq? '/ (operator lis)) (quotient (M_integer(operand1 lis)) (M_integer(operand2 lis)))]
@@ -30,6 +43,9 @@
       [(eq? '!     (operator lis))    (not(M_boolean (operand1 lis))) ]
       [else 'nooperator])))
 
+;(define M_value
+;  (lambda (lis)))
+
 ;; Comparison ops
 
 (define M_comparison
@@ -44,26 +60,42 @@
       [(eq? '>     (operator lis))    (> (M_comparison(operand2 lis)) (M_comparison(operand3 lis)))]
       [else 'nooperator])))
 
-;; TODO statements
+;; TODO statements - currently tentative i think M_value is needed to do these properly
 
 (define M_assign
-  (lambda (lis)
-    (cond
-      [(null? lis) '()]
-      [(eq? '= (operator lis)) (cons operand1 operand2)]
-      [else 'nooperator])))
+  (lambda (lis state)
+    (if (eq? '= (operator lis))
+       (setBinding (cons operand1 (cons operand2 '())) state)
+       'nooperator)))
 
 (define M_declare
   (lambda (lis state)
+    (if (eq? 'var (operator lis))
+      (cons (cons operand1 '()) state)
+      'nooperator)))
+
+(define M_if
+  (lambda (lis state)
     (cond
-      [(null? lis) '()]
-      [(eq? 'var (operator lis)) (cons operand1 state)]
+      [(operand1) operand2] 
+      [(eq? operand3 'else) (cdr operand3)]
       [else 'nooperator])))
+
+(define M_while
+  (lambda (lis state)
+    (cond
+      [(operand1) operand2] ;;need to evaluate repeatedly not sure how
+      [else 'nooperator])))
+
+
+
+
+
 
 (define getBinding ;; takes a variable name and a state
   (lambda (x state)
     (cond
-      [(null? state) 'noSuchBinding] ;; change to error later
+      [(null? state) (error 'noSuchBinding x)] ;; change to error later
       [(eq? x (car (car state))) (car state)]
       [else                       (getBinding x (cdr state))])))
 
@@ -71,14 +103,14 @@
   (lambda (x state)
     (cond
       [(null? state)                                      '()]
-      [(and (list? (car state)) (eq? (getBinding (car x) state) 'noSuchBinding))    (error 'noSuchVariable)]
+      [(and (list? (car state)) (eq? (getBinding (car x) state) 'noSuchBinding))    (error 'noSuchVariable (car x))]
       [(list? (car state))                                (cons (setBinding x (car state)) (setBinding x (cdr state)))]
       [(eq? (car x) (car state))                          (cons (car state) (cons (cadr x) '())) ]
       [else                                               (cons (car state) (setBinding x (cdr state)))])))
         
     
-
 ;; store the state in the stack, use tail recursion to continuously read and alter the state as you recursively go through the program
+
 ; state
 
 (define M_state
@@ -93,8 +125,13 @@
 
 
 
+(define program (lambda (file) (parser file)))
 
-
+(define interpret
+  (lambda (filename)
+    (cond
+      [(eq? (operator (car program)) 'var)]
+      [else 'lol])))
 
 
 
